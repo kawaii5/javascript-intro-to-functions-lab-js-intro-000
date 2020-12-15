88b465c91c8d4dbb7b9444477a6bee52072047e1
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-15 04:20:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = string.toUpperCase()
   var lowercase = string.toUpperCase()
-  if (string === lowercase) {
+  if (string == lowercase) {
     return "I can't hear you!"
-  } else if (string === uppercase) {
+  } else if (string == uppercase) {
     return "YES INDEED!"
   } else if (string == "I love you, Grandma.") {
     return "I love you, too."
